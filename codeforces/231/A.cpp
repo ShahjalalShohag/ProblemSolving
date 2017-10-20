e4a5810b6a4c9d4edce21a9f808fdbe073b51124
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2017-10-20 23:55:00 +0600

    Add solution for problem `A - Team` Link: https://codeforces.com/contest/231/problem/A Tags: brute force, greedy, *800 Ref: https://codeforces.com/contest/231/submission/31542088

---
+++ codeforces/231/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n,k,a,cnt,count=0;
+    cin>>n;
+    while(n--){
+            k=3;
+            cnt=0;
+        while(k--){
+            cin>>a;
+    if(a==1) cnt++;
+        }
+        if(cnt>1) count++;
+    }
+    cout<<count<<endl;
+    return 0;
+}
