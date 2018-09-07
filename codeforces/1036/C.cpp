7a37b6b62e1ec5ecca130c30b3e11337ab79320c
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2018-09-08 01:18:00 +0600

    Add solution for problem `C - Classy Numbers` Link: https://codeforces.com/contest/1036/problem/C Tags: combinatorics, dp, *1900 Ref: https://codeforces.com/contest/1036/submission/42652341

---
+++ codeforces/1036/C.cpp
@@ -0,0 +1,118 @@
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpll vector<pll>
+#define vpii vector<pii>
+#define umap unordered_map
+#define uset unordered_set
+#define PQ priority_queue
+
+#define printa(a,L,R) for(int i=L;i<R;i++) cout<<a[i]<<(i==R-1?'\n':' ')
+#define printv(a) printa(a,0,a.size())
+#define print2d(a,r,c) for(int i=0;i<r;i++) for(int j=0;j<c;j++) cout<<a[i][j]<<(j==c-1?'\n':' ')
+#define pb push_back
+#define eb emplace_back
+#define mt make_tuple
+#define fbo find_by_order
+#define ook order_of_key
+#define MP make_pair
+#define UB upper_bound
+#define LB lower_bound
+#define SQ(x) ((x)*(x))
+#define issq(x) (((ll)(sqrt((x))))*((ll)(sqrt((x))))==(x))
+#define F first
+#define S second
+#define mem(a,x) memset(a,x,sizeof(a))
+#define inf 0x3f3f3f3f
+#define PI (acos(-1.0))
+#define E 2.71828182845904523536
+#define gamma 0.5772156649
+#define nl "\n"
+#define lg(r,n) (int)(log2(n)/log2(r))
+#define pf printf
+#define sf scanf
+#define _ccase printf("Case %lld: ",++cs)
+#define _case cout<<"Case "<<++cs<<": "
+#define by(x) [](const auto& a, const auto& b) { return a.x < b.x; }
+
+#define asche cerr<<"Ekhane asche\n";
+#define rev(v) reverse(v.begin(),v.end())
+#define srt(v) sort(v.begin(),v.end())
+#define grtsrt(v) sort(v.begin(),v.end(),greater<int>())
+#define all(v) v.begin(),v.end()
+#define mnv(v) *min_element(v.begin(),v.end())
+#define mxv(v) *max_element(v.begin(),v.end())
+#define toint(a) atoi(a.c_str())
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define valid(tx,ty) (tx>=0&&tx<n&&ty>=0&&ty<m)
+#define dist(x1,y1,x2,y2) sqrt(SQ(x1-x2)+SQ(y1-y2))
+#define one(x) __builtin_popcountll(x)
+#define Unique(v) v.erase(unique(all(v)),v.end())
+#define stree ll l=(n<<1),r=l+1,mid=b+(e-b)/2
+string tostr(int n) {stringstream rr;rr<<n;return rr.str();}
+template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//ll dx[]={1,0,-1,0,1,-1,-1,1};
+//ll dy[]={0,1,0,-1,1,1,-1,-1};
+
+const int mod=1e9+7;
+const int mxn=1e5+9;
+const ld eps=1e-9;
+//ll gcd(ll a,ll b){while(b){ll x=a%b;a=b;b=x;}return a;}
+//ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+//ll qpow(ll n,ll k) {ll ans=1;assert(k>=0);while(k>0){if(k&1) ans=(ans*n)%mod;n=(n*n)%mod;k>>=1;}return ans%mod;}
+vll digit;
+ll sz,dp[20][20][2];
+ll yo(ll idx,ll cnt,bool badha)
+{
+    if(idx==-1){
+        if(cnt<=3) return 1;
+        else return 0;
+    }
+    ll &ret=dp[idx][cnt][badha];
+    if(ret!=-1&&badha!=1) return ret;
+    ll ans=0;
+    ll mxhere=badha?digit[idx]:9;
+    for(ll i=0;i<=mxhere;i++){
+        bool next_badha=(i==digit[idx]?badha:0);
+        ans+=yo(idx-1,(i==0?cnt:cnt+1),next_badha);
+    }
+    if(badha==0) ret=ans;
+    return ans;
+}
+ll get(ll n)
+{
+    if(n<0) return 0;
+    digit.clear();
+    while(n) digit.pb(n%10),n/=10;
+    sz=digit.size();
+    return yo(sz-1,0,1);
+}
+int main()
+{
+    fast;
+    ll i,j,k,n,m,l,r,t;
+    mem(dp,-1);
+    cin>>t;
+    while(t--){
+        cin>>l>>r;
+        cout<<get(r)-get(l-1)<<nl;
+    }
+    return 0;
+}
