2ffb3728b4a15ec6b4a7a8d8413b9b26c045ce79
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2018-11-28 02:23:00 +0600

    Add solution for problem `G - Bandit Blues` Link: https://codeforces.com/contest/960/problem/G Tags: combinatorics, dp, fft, math, *2900 Ref: https://codeforces.com/contest/960/submission/46291834

--- codeforces/960/G.cpp
+++ codeforces/960/G.cpp
@@ -1,3 +1,8 @@
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
@@ -96,166 +101,173 @@ const ld PI=acos(-1.0);
 //ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
 //ll qpow(ll n,ll k) {ll ans=1;assert(k>=0);n%=mod;while(k>0){if(k&1) ans=(ans*n)%mod;n=(n*n)%mod;k>>=1;}return ans%mod;}
 
-///Don't use pragma before using this
-namespace ntt
+template<class T, class T2> inline void chkmax(T &x, const T2 &y) { if(x < y) x = y; }
+template<class T, class T2> inline void chkmin(T &x, const T2 &y) { if(x > y) x = y; }
+const int MAXN = N;
+
+inline void addmod(int& x, int y, int mod) { (x += y) >= mod && (x -= mod); }
+inline int mulmod(int x, int y, int mod) { return x * 1ll * y % mod; }
+
+struct complex_base
 {
-    struct num
-    {
-        double x,y;
-        num() {x=y=0;}
-        num(double x,double y):x(x),y(y){}
-    };
-    inline num operator+(num a,num b) {return num(a.x+b.x,a.y+b.y);}
-    inline num operator-(num a,num b) {return num(a.x-b.x,a.y-b.y);}
-    inline num operator*(num a,num b) {return num(a.x*b.x-a.y*b.y,a.x*b.y+a.y*b.x);}
-    inline num conj(num a) {return num(a.x,-a.y);}
-
-    int base=1;
-    vector<num> roots={{0,0},{1,0}};
-    vector<int> rev={0,1};
-    const double PI=acosl(-1.0);
-
-    void ensure_base(int nbase)
-    {
-        if(nbase<=base) return;
-        rev.resize(1<<nbase);
-        for(int i=0;i<(1<<nbase);i++)
-            rev[i]=(rev[i>>1]>>1)+((i&1)<<(nbase-1));
-        roots.resize(1<<nbase);
-        while(base<nbase)
-        {
-            double angle=2*PI/(1<<(base+1));
-            for(int i=1<<(base-1);i<(1<<base);i++)
-            {
-                roots[i<<1]=roots[i];
-                double angle_i=angle*(2*i+1-(1<<base));
-                roots[(i<<1)+1]=num(cos(angle_i),sin(angle_i));
-            }
-            base++;
-        }
-    }
-
-    void fft(vector<num> &a,int n=-1)
-    {
-        if(n==-1) n=a.size();
-        assert((n&(n-1))==0);
-        int zeros=__builtin_ctz(n);
-        ensure_base(zeros);
-        int shift=base-zeros;
-        for(int i=0;i<n;i++)
-            if(i<(rev[i]>>shift))
-                swap(a[i],a[rev[i]>>shift]);
-        for(int k=1;k<n;k<<=1)
-        {
-            for(int i=0;i<n;i+=2*k)
-            {
-                for(int j=0;j<k;j++)
-                {
-                    num z=a[i+j+k]*roots[j+k];
-                    a[i+j+k]=a[i+j]-z;
-                    a[i+j]=a[i+j]+z;
-                }
-            }
-        }
-    }
-
-    vector<num> fa,fb;
-
-    vector<int> multiply(vector<int> &a, vector<int> &b)
-    {
-        int need=a.size()+b.size()-1;
-        int nbase=0;
-        while((1<<nbase)<need) nbase++;
-        ensure_base(nbase);
-        int sz=1<<nbase;
-        if(sz>(int)fa.size()) fa.resize(sz);
-        for(int i=0;i<sz;i++)
-        {
-            int x=(i<(int)a.size()?a[i]:0);
-            int y=(i<(int)b.size()?b[i]:0);
-            fa[i]=num(x,y);
-        }
-        fft(fa,sz);
-        num r(0,-0.25/sz);
-        for(int i=0;i<=(sz>>1);i++)
-        {
-            int j=(sz-i)&(sz-1);
-            num z=(fa[j]*fa[j]-conj(fa[i]*fa[i]))*r;
-            if(i!=j) fa[j]=(fa[i]*fa[i]-conj(fa[j]*fa[j]))*r;
-            fa[i]=z;
-        }
-        fft(fa,sz);
-        vector<int> res(need);
-        for(int i=0;i<need;i++) res[i]=fa[i].x+0.5;
-        return res;
-    }
-
-    vector<int> multiply(vector<int> &a,vector<int> &b,int m,int eq=0)
-    {
-        int need=a.size()+b.size()-1;
-        int nbase=0;
-        while((1<<nbase)<need) nbase++;
-        ensure_base(nbase);
-        int sz=1<<nbase;
-        if(sz>(int)fa.size()) fa.resize(sz);
-        for(int i=0;i<(int)a.size();i++)
-        {
-            int x=(a[i]%m+m)%m;
-            fa[i]=num(x&((1<<15)-1),x>>15);
-        }
-        fill(fa.begin()+a.size(),fa.begin()+sz,num{0,0});
-        fft(fa,sz);
-        if(sz>(int)fb.size()) fb.resize(sz);
-        if(eq) copy(fa.begin(),fa.begin()+sz,fb.begin());
-        else
-        {
-            for(int i=0;i<(int)b.size();i++)
-            {
-                int x=(b[i]%m+m)%m;
-                fb[i]=num(x&((1<<15)-1),x>>15);
-            }
-            fill(fb.begin()+b.size(),fb.begin()+sz,num{0,0});
-            fft(fb,sz);
-        }
-        double ratio=0.25/sz;
-        num r2(0,-1),r3(ratio,0),r4(0,-ratio),r5(0,1);
-        for(int i=0;i<=(sz>>1);i++)
-        {
-            int j=(sz-i)&(sz-1);
-            num a1=(fa[i]+conj(fa[j]));
-            num a2=(fa[i]-conj(fa[j]))*r2;
-            num b1=(fb[i]+conj(fb[j]))*r3;
-            num b2=(fb[i]-conj(fb[j]))*r4;
-            if(i!=j)
-            {
-                num c1=(fa[j]+conj(fa[i]));
-                num c2=(fa[j]-conj(fa[i]))*r2;
-                num d1=(fb[j]+conj(fb[i]))*r3;
-                num d2=(fb[j]-conj(fb[i]))*r4;
-                fa[i]=c1*d1+c2*d2*r5;
-                fb[i]=c1*d2+c2*d1;
-            }
-            fa[j]=a1*b1+a2*b2*r5;
-            fb[j]=a1*b2+a2*b1;
-        }
-        fft(fa,sz);fft(fb,sz);
-        vector<int> res(need);
-        for(int i=0;i<need;i++)
-        {
-            ll aa=fa[i].x+0.5;
-            ll bb=fb[i].x+0.5;
-            ll cc=fa[i].y+0.5;
-            res[i]=(aa+((bb%m)<<15)+((cc%m)<<30))%m;
-        }
-        return res;
-    }
-    vector<int> square(vector<int> &a,int m)
-    {
-        return multiply(a,a,m,1);
-    }
+	double x, y;
+	complex_base(double _x = 0, double _y = 0) { x = _x; y = _y; }
+	friend complex_base operator-(const complex_base &a, const complex_base &b) { return complex_base(a.x - b.x, a.y - b.y); }
+	friend complex_base operator+(const complex_base &a, const complex_base &b) { return complex_base(a.x + b.x, a.y + b.y); }
+	friend complex_base operator*(const complex_base &a, const complex_base &b) { return complex_base(a.x * b.x - a.y * b.y, a.y * b.x + b.y * a.x); }
+	friend void operator/=(complex_base &a, const double &P) { a.x /= P; a.y /= P; }
 };
-using namespace ntt;
 
+int bit_rev[MAXN];
+int last_n_fft = -1, ilast_n_fft = -1;
+complex_base root[MAXN], iroot[MAXN];
+
+void fft(complex_base *a, int lg)
+{
+	int n = (1 << lg);
+	if(last_n_fft != n)
+	{
+		double ang = 2 * PI / n;
+		for(int i = 0; i < (n >> 1); i++)
+			root[i] = complex_base(cos(ang * i), sin(ang * i));
+
+		last_n_fft = n;
+	}
+
+	for(int i = 1; i < n; i++)
+	{
+		bit_rev[i] = (bit_rev[i >> 1] >> 1) | ((i & 1) << (lg - 1));
+		if(bit_rev[i] < i) swap(a[i], a[bit_rev[i]]);
+	}
+
+	for(int len = 2; len <= n; len <<= 1)
+	{
+		int step = (n / len);
+		for(int j = 0; j < (len >> 1); j++)
+			for(int i = 0; i < n; i += len)
+			{
+				complex_base u = a[i + j], v = root[step * j] * a[i + j + (len >> 1)];
+				a[i + j] = u + v;
+				a[i + j + (len >> 1)] = u - v;
+			}
+	}
+}
+
+void inv_fft(complex_base *a, int lg)
+{
+	int n = (1 << lg);
+	if(ilast_n_fft != n)
+	{
+		double ang = -2 * PI / n;
+		for(int i = 0; i < (n >> 1); i++)
+			iroot[i] = complex_base(cos(ang * i), sin(ang * i));
+
+		ilast_n_fft = n;
+	}
+
+	for(int i = 1; i < n; i++)
+	{
+		bit_rev[i] = (bit_rev[i >> 1] >> 1) | ((i & 1) << (lg - 1));
+		if(bit_rev[i] < i) swap(a[i], a[bit_rev[i]]);
+	}
+
+	for(int len = 2; len <= n; len <<= 1)
+	{
+		int step = (n / len);
+		for(int j = 0; j < (len >> 1); j++)
+			for(int i = 0; i < n; i += len)
+			{
+				complex_base u = a[i + j], v = iroot[step * j] * a[i + j + (len >> 1)];
+				a[i + j] = u + v;
+				a[i + j + (len >> 1)] = u - v;
+			}
+	}
+
+	for(int i = 0; i < n; i++)
+		a[i] /= n;
+}
+
+complex_base A[MAXN], B[MAXN];
+
+vector<int> mult(const vector<int> &a, const vector<int> &b)
+{
+	if(a.size() * b.size() <= 128)
+	{
+		vector<int> ans(a.size() + b.size(), 0);
+		for(int i = 0; i < (int)a.size(); i++)
+			for(int j = 0; j < (int)b.size(); j++)
+				ans[i + j] = (ans[i + j] + a[i] * 1ll * b[j]) % mod;
+
+		return ans;
+	}
+
+	int lg = 0; while((1 << lg) < (int)(a.size() + b.size())) ++lg;
+	for(int i = 0; i < (1 << lg); i++) A[i] = B[i] = complex_base(0, 0);
+	for(int i = 0; i < (int)a.size(); i++) A[i] = complex_base(a[i], 0);
+	for(int i = 0; i < (int)b.size(); i++) B[i] = complex_base(b[i], 0);
+
+	fft(A, lg); fft(B, lg);
+	for(int i = 0; i < (1 << lg); i++)
+		A[i] = A[i] * B[i];
+	inv_fft(A, lg);
+
+	vector<int> ans(a.size() + b.size(), 0);
+	for(int i = 0; i < (int)ans.size(); i++)
+		ans[i] = (int64_t)(A[i].x + 0.5) % mod;
+
+	return ans;
+}
+
+vector<int> mult_mod(const vector<int> &a, const vector<int> &b)
+{
+	// a = a0 + sqrt(MOD) * a1
+	// a = a0 + base * a1
+	int base = (int)sqrtl(mod);
+
+	vector<int> a0(a.size()), a1(a.size());
+	for(int i = 0; i < (int)a.size(); i++)
+	{
+		a0[i] = a[i] % base;
+		a1[i] = a[i] / base;
+	}
+
+	vector<int> b0(b.size()), b1(b.size());
+	for(int i = 0; i < (int)b.size(); i++)
+	{
+		b0[i] = b[i] % base;
+		b1[i] = b[i] / base;
+	}
+
+	vector<int> a01 = a0;
+	for(int i = 0; i < (int)a.size(); i++)
+		addmod(a01[i], a1[i], mod);
+
+	vector<int> b01 = b0;
+	for(int i = 0; i < (int)b.size(); i++)
+		addmod(b01[i], b1[i], mod);
+
+	vector<int> C = mult(a01, b01);  // 1
+
+	vector<int> a0b0 = mult(a0, b0); // 2
+	vector<int> a1b1 = mult(a1, b1); // 3
+
+	vector<int> mid = C;
+	for(int i = 0; i < (int)mid.size(); i++)
+	{
+		addmod(mid[i], -a0b0[i] + mod, mod);
+		addmod(mid[i], -a1b1[i] + mod, mod);
+	}
+
+	vector<int> res = a0b0;
+	for(int i = 0; i < (int)res.size(); i++)
+		addmod(res[i], mulmod(base, mid[i], mod), mod);
+
+	base = mulmod(base, base, mod);
+	for(int i = 0; i < (int)res.size(); i++)
+		addmod(res[i], mulmod(base, a1b1[i], mod), mod);
+
+	return res;
+}
 
 vi vec[N];
 int stirling(int n,int k)
@@ -271,7 +283,7 @@ int stirling(int n,int k)
     for(int i=n;i<mx;i++) vec[i].eb(1);
     for(int j=mx;j>1;j>>=1){
         int d=j>>1;
-        for(int i=0;i<d;i++) vec[i]=multiply(vec[i],vec[i+d],mod);
+        for(int i=0;i<d;i++) vec[i]=mult_mod(vec[i],vec[i+d]);
     }
     if(k>=vec[0].size()) return 0;
     return vec[0][k];
