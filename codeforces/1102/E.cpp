ed7a3e58b791e5df1fd2ea275ab303b0ab43f63d
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2019-01-10 01:04:00 +0600

    Add solution for problem `E - Monotonic Renumeration` Link: https://codeforces.com/contest/1102/problem/E Tags: combinatorics, sortings, *1700 Ref: https://codeforces.com/contest/1102/submission/48160161

--- codeforces/1102/E.cpp
+++ codeforces/1102/E.cpp
@@ -97,26 +97,13 @@ const ld PI=acos(-1.0);
 //ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
 //ll qpow(ll n,ll k) {ll ans=1;assert(k>=0);n%=mod;while(k>0){if(k&1) ans=(ans*n)%mod;n=(n*n)%mod;k>>=1;}return ans%mod;}
 
-struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
 
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
 int a[N];
-umap<int,int,custom_hash>r;
+map<int,int>r;
 int main()
 {
     BeatMeScanf;
     int i,j,k,n,m;
-    r.reserve(1<<20);
     cin>>n;
     for(i=1;i<=n;i++) cin>>a[i],r[a[i]]=i;
     int cnt=0;
