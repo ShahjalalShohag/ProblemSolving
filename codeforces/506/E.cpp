8ec32d5667c4a0b14f1de1c650a5bfe71df9a880
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2019-03-19 19:05:00 +0600

    Add solution for problem `E - Mr. Kitayuta's Gift` Link: https://codeforces.com/contest/506/problem/E Tags: combinatorics, dp, matrices, strings, *3000 Ref: https://codeforces.com/contest/506/submission/51484697

---
+++ codeforces/506/E.cpp
@@ -0,0 +1,207 @@
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpll vector<pll>
+#define vpii vector<pii>
+#define umap unordered_map
+#define uset unordered_set
+#define PQ priority_queue
+
+#define printa(a,L,R) for(int i=L;i<R;i++) cout<<a[i]<<(i==R-1?'\n':' ')
+#define printv(a) printa(a,0,a.size())
+#define print2d(a,r,c) for(int i=0;i<r;i++) for(int j=0;j<c;j++) cout<<a[i][j]<<(j==c-1?'\n':' ')
+#define pb push_back
+#define eb emplace_back
+#define mt make_tuple
+#define fbo find_by_order
+#define ook order_of_key
+#define MP make_pair
+#define UB upper_bound
+#define LB lower_bound
+#define SQ(x) ((x)*(x))
+#define issq(x) (((ll)(sqrt((x))))*((ll)(sqrt((x))))==(x))
+#define F first
+#define S second
+#define mem(a,x) memset(a,x,sizeof(a))
+#define E 2.71828182845904523536
+#define gamma 0.5772156649
+#define nl "\n"
+#define lg(r,n) (int)(log2(n)/log2(r))
+#define sz(v) (int)v.size()
+#define pf printf
+#define sf scanf
+#define sf1(a)                scanf("%d",&a)
+#define sf2(a,b)              scanf("%d %d",&a,&b)
+#define sf3(a,b,c)            scanf("%d %d %d",&a,&b,&c)
+#define pf1(a)                printf("%d\n",a);
+#define pf2(a,b)              printf("%d %d\n",a,b)
+#define pf3(a,b,c)            printf("%d %d %d\n",a,b,c)
+#define sf1ll(a)              scanf("%lld",&a)
+#define sf2ll(a,b)            scanf("%I64d %I64d",&a,&b)
+#define sf3ll(a,b,c)          scanf("%I64d %I64d %I64d",&a,&b,&c)
+#define pf1ll(a)              printf("%lld\n",a);
+#define pf2ll(a,b)            printf("%I64d %I64d\n",a,b)
+#define pf3ll(a,b,c)          printf("%I64d %I64d %I64d\n",a,b,c)
+#define _ccase printf("Case %lld: ",++cs)
+#define _case cout<<"Case "<<++cs<<": "
+#define by(x) [](const auto& a, const auto& b) { return a.x < b.x; }
+
+#define asche cerr<<"Ekhane asche\n";
+#define rev(v) reverse(v.begin(),v.end())
+#define srt(v) sort(v.begin(),v.end())
+#define grtsrt(v) sort(v.begin(),v.end(),greater<ll>())
+#define all(v) v.begin(),v.end()
+#define mnv(v) *min_element(v.begin(),v.end())
+#define mxv(v) *max_element(v.begin(),v.end())
+#define toint(a) atoi(a.c_str())
+#define BeatMeScanf ios_base::sync_with_stdio(false)
+#define valid(tx,ty) (tx>=0&&tx<n&&ty>=0&&ty<m)
+#define one(x) __builtin_popcount(x)
+#define Unique(v) v.erase(unique(all(v)),v.end())
+#define stree l=(n<<1),r=l+1,mid=b+(e-b)/2
+#define fout(x) fixed<<setprecision(x)
+string tostr(int n) {stringstream rr;rr<<n;return rr.str();}
+inline void yes(){cout<<"YES\n";exit(0);}
+inline void no(){cout<<"NO\n";exit(0);}
+template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ll dx[]={1,0,-1,0,1,-1,-1,1};
+ll dy[]={0,1,0,-1,1,1,-1,-1};
+//random_device rd;
+//mt19937 rnd(rd());
+#define debug(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); deb(_it, args); }
+void deb(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void deb(istream_iterator<string> it, T a, Args... args) {
+    cerr << *it << " = " << a << endl;
+    deb(++it, args...);
+}
+
+const int mod=1e4+7;
+const int N=3e5+9;
+const ld eps=1e-9;
+const ld PI=acos(-1.0);
+ll gcd(ll a,ll b){while(b){ll x=a%b;a=b;b=x;}return a;}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+ll qpow(ll n,ll k) {ll ans=1;assert(k>=0);n%=mod;while(k>0){if(k&1) ans=(ans*n)%mod;n=(n*n)%mod;k>>=1;}return ans%mod;}
+///Description: n-order recurrence has following recurrence a[i]=f(i-1)*a[i-1]+f(i-2)*a[i-2]+...f(i-n)*a[i-n]
+///this algo Recovers shortest n-order linear recurrence relation from the first
+///2n terms of the recurrence.
+///Useful for guessing linear recurrences after brute-forcing the first terms.
+///Should work on any field, but numerical stability for floats is not guaranteed.
+///Output will have size <=2*n.
+vector<ll> BerlekampMassey(vector<ll> s) {
+	int n = sz(s), L = 0, m = 0;
+	vector<ll> C(n), B(n), T;
+	C[0] = B[0] = 1;
+
+	ll b = 1;
+	for(int i=0;i<n;i++) {
+        ++m;
+		ll d = s[i] % mod;
+		for(int j=1;j<=L;j++) d = (d + C[j] * s[i - j]) % mod;
+		if (!d) continue;
+		T = C; ll coef = d * qpow(b, mod-2) % mod;
+		for(int j=m;j<n;j++) C[j] = (C[j] - coef * B[j - m]) % mod;
+		if (2 * L > i) continue;
+		L = i + 1 - L; B = T; b = d; m = 0;
+	}
+
+	C.resize(L + 1); C.erase(C.begin());
+	for(auto& x:C)  x = (mod - x) % mod;
+	return C;
+}
+///transition-> for(i=0;i<x;i++) f[n]=tr[i]*f[n-i-1]
+///S=starting recurrence
+///k is 0 indexed
+long long linearRec(vector<long long> S, vector<long long> tr, long long k) {
+	long long n = S.size();
+
+	auto combine = [&](vector<long long> a, vector<long long> b) {
+		vector<long long> res(n * 2 + 1);
+		for (long long i=0; i<n+1; i++) for (long long j=0; j<n+1; j++)
+			res[i + j] = (res[i + j] + a[i] * b[j]) % mod;
+		for (long long i = 2 * n; i > n; --i) for (long long j=0; j<n; j++)
+			res[i - 1 - j] = (res[i - 1 - j] + res[i] * tr[j]) % mod;
+		res.resize(n + 1);
+		return res;
+	};
+
+	vector<long long> pol(n + 1), e(pol);
+	pol[0] = e[1] = 1;
+
+	for (++k; k; k /= 2) {
+		if (k % 2) pol = combine(pol, e);
+		e = combine(e, e);
+	}
+
+	long long res = 0;
+	for (long long i=0; i<n; i++) res = (res + pol[i + 1] * S[i]) % mod;
+	return res;
+}
+int dp[205][205][2010],n;
+string s;
+int yo(int i,int j,int k)
+{
+    if(k<0) return 0;
+    if(k==0&&i>=j) return 1;
+    int &ret=dp[i][j][k];
+    if(ret!=-1) return ret;
+    if(k==0) return ret=(s[i]==s[j])&&yo(i+1,j-1,0);
+    ret=0;
+    if(i>j){
+        int p=min(k,2);
+        ret+=26*yo(i,j,k-p);
+    }
+    else if(i==j){
+        ret+=yo(i+1,j,k-1);
+        ret+=25*yo(i,j,k-2);
+    }
+    else{
+        if(s[i]==s[j]){
+            ret+=yo(i+1,j-1,k);
+            ret+=25*yo(i,j,k-2);
+        }
+        else{
+            ret+=yo(i,j-1,k-1);
+            ret+=yo(i+1,j,k-1);
+            ret+=24*yo(i,j,k-2);
+        }
+    }
+    ret%=mod;
+    return ret;
+}
+int32_t main()
+{
+    BeatMeScanf;
+    int i,j,k,m;
+    cin>>s>>k;
+    n=s.size();
+    mem(dp,-1);
+    vll v;
+    for(i=0;i<=2000;i++) v.eb(yo(0,n-1,i));
+    vll tr=BerlekampMassey(v);
+    v.resize(sz(tr));
+    cout<<linearRec(v,tr,k)<<nl;
+    return 0;
+}
+///Before submit=>
+///    *check for integer overflow,array bounds
+///    *check for n=1
+
+
