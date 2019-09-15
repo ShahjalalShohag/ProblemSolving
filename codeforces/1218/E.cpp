ec03460a3ab792ca88cfadcf451292f7169c07ed
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2019-09-15 19:53:00 +0600

    Add solution for problem `E - Product Tuples` Link: https://codeforces.com/contest/1218/problem/E Tags: divide and conquer, fft, *2500 Ref: https://codeforces.com/contest/1218/submission/60636049

---
+++ codeforces/1218/E.cpp
@@ -0,0 +1,265 @@
+//#pragma comment(linker, "/stack:200000000")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma GCC optimize("unroll-loops")
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define vpii vector<pii>
+#define pb push_back
+#define eb emplace_back
+#define mt make_tuple
+#define MP make_pair
+#define fs first
+#define sc second
+#define F first
+#define S second
+#define nl "\n"
+#define asche cerr<<"Ekhane asche\n";
+#define pf printf
+#define sf scanf
+#define mem(a,x) memset(a,x,sizeof(a))
+#define SZ(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define one(x) __builtin_popcount(x)
+#define fout(x) fixed<<setprecision(x)
+#define BeatMeScanf ios_base::sync_with_stdio(false)
+//int dx[]={1,0,-1,0,1,-1,-1,1};
+//int dy[]={0,1,0,-1,1,1,-1,-1};
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+#define debug(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); deb(_it, args); }
+void deb(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void deb(istream_iterator<string> it, T a, Args... args) {
+    cerr << *it << " = " << a << endl;
+    deb(++it, args...);
+}
+//template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+namespace IO
+{
+    #define in() ( { int a ; scanf("%d",&a); a; } )
+    #define LL() ( { ll a ; scanf("%lld",&a); a; } )
+    #define ULL() ( { ull a ; scanf("%llu",&a); a; } )
+    #define DD() ({ double a; scanf("%lf", &a); a;})
+    #define CC() ( { char a ; scanf("%c",&a); a; } )
+    #define pf1(a) printf("%d\n",a)
+    #define pf1ll(a) printf("%lld\n",a)
+}
+using namespace IO;
+
+const int mod=998244353 ;
+const int N=2e4+9;
+const double eps=1e-9;
+const double PI=acos(-1.0);
+ll qpow(ll n,ll k) {ll ans=1;assert(k>=0);n%=mod;while(k>0){if(k&1) ans=(ans*n)%mod;n=(n*n)%mod;k>>=1;}return ans%mod;}
+
+///Don't use pragma before using this
+namespace ntt
+{
+    struct num
+    {
+        double x,y;
+        num() {x=y=0;}
+        num(double x,double y):x(x),y(y){}
+    };
+    inline num operator+(num a,num b) {return num(a.x+b.x,a.y+b.y);}
+    inline num operator-(num a,num b) {return num(a.x-b.x,a.y-b.y);}
+    inline num operator*(num a,num b) {return num(a.x*b.x-a.y*b.y,a.x*b.y+a.y*b.x);}
+    inline num conj(num a) {return num(a.x,-a.y);}
+
+    int base=1;
+    vector<num> roots={{0,0},{1,0}};
+    vector<int> rev={0,1};
+    const double PI=acosl(-1.0);
+
+    void ensure_base(int nbase)
+    {
+        if(nbase<=base) return;
+        rev.resize(1<<nbase);
+        for(int i=0;i<(1<<nbase);i++)
+            rev[i]=(rev[i>>1]>>1)+((i&1)<<(nbase-1));
+        roots.resize(1<<nbase);
+        while(base<nbase)
+        {
+            double angle=2*PI/(1<<(base+1));
+            for(int i=1<<(base-1);i<(1<<base);i++)
+            {
+                roots[i<<1]=roots[i];
+                double angle_i=angle*(2*i+1-(1<<base));
+                roots[(i<<1)+1]=num(cos(angle_i),sin(angle_i));
+            }
+            base++;
+        }
+    }
+
+    void fft(vector<num> &a,int n=-1)
+    {
+        if(n==-1) n=a.size();
+        assert((n&(n-1))==0);
+        int zeros=__builtin_ctz(n);
+        ensure_base(zeros);
+        int shift=base-zeros;
+        for(int i=0;i<n;i++)
+            if(i<(rev[i]>>shift))
+                swap(a[i],a[rev[i]>>shift]);
+        for(int k=1;k<n;k<<=1)
+        {
+            for(int i=0;i<n;i+=2*k)
+            {
+                for(int j=0;j<k;j++)
+                {
+                    num z=a[i+j+k]*roots[j+k];
+                    a[i+j+k]=a[i+j]-z;
+                    a[i+j]=a[i+j]+z;
+                }
+            }
+        }
+    }
+
+    vector<num> fa,fb;
+
+    vector<int> multiply(vector<int> &a, vector<int> &b)
+    {
+        int need=a.size()+b.size()-1;
+        int nbase=0;
+        while((1<<nbase)<need) nbase++;
+        ensure_base(nbase);
+        int sz=1<<nbase;
+        if(sz>(int)fa.size()) fa.resize(sz);
+        for(int i=0;i<sz;i++)
+        {
+            int x=(i<(int)a.size()?a[i]:0);
+            int y=(i<(int)b.size()?b[i]:0);
+            fa[i]=num(x,y);
+        }
+        fft(fa,sz);
+        num r(0,-0.25/sz);
+        for(int i=0;i<=(sz>>1);i++)
+        {
+            int j=(sz-i)&(sz-1);
+            num z=(fa[j]*fa[j]-conj(fa[i]*fa[i]))*r;
+            if(i!=j) fa[j]=(fa[i]*fa[i]-conj(fa[j]*fa[j]))*r;
+            fa[i]=z;
+        }
+        fft(fa,sz);
+        vector<int> res(need);
+        for(int i=0;i<need;i++) res[i]=fa[i].x+0.5;
+        return res;
+    }
+
+    vector<int> multiply(vector<int> &a,vector<int> &b,int m,int eq=0)
+    {
+        int need=a.size()+b.size()-1;
+        int nbase=0;
+        while((1<<nbase)<need) nbase++;
+        ensure_base(nbase);
+        int sz=1<<nbase;
+        if(sz>(int)fa.size()) fa.resize(sz);
+        for(int i=0;i<(int)a.size();i++)
+        {
+            int x=(a[i]%m+m)%m;
+            fa[i]=num(x&((1<<15)-1),x>>15);
+        }
+        fill(fa.begin()+a.size(),fa.begin()+sz,num{0,0});
+        fft(fa,sz);
+        if(sz>(int)fb.size()) fb.resize(sz);
+        if(eq) copy(fa.begin(),fa.begin()+sz,fb.begin());
+        else
+        {
+            for(int i=0;i<(int)b.size();i++)
+            {
+                int x=(b[i]%m+m)%m;
+                fb[i]=num(x&((1<<15)-1),x>>15);
+            }
+            fill(fb.begin()+b.size(),fb.begin()+sz,num{0,0});
+            fft(fb,sz);
+        }
+        double ratio=0.25/sz;
+        num r2(0,-1),r3(ratio,0),r4(0,-ratio),r5(0,1);
+        for(int i=0;i<=(sz>>1);i++)
+        {
+            int j=(sz-i)&(sz-1);
+            num a1=(fa[i]+conj(fa[j]));
+            num a2=(fa[i]-conj(fa[j]))*r2;
+            num b1=(fb[i]+conj(fb[j]))*r3;
+            num b2=(fb[i]-conj(fb[j]))*r4;
+            if(i!=j)
+            {
+                num c1=(fa[j]+conj(fa[i]));
+                num c2=(fa[j]-conj(fa[i]))*r2;
+                num d1=(fb[j]+conj(fb[i]))*r3;
+                num d2=(fb[j]-conj(fb[i]))*r4;
+                fa[i]=c1*d1+c2*d2*r5;
+                fb[i]=c1*d2+c2*d1;
+            }
+            fa[j]=a1*b1+a2*b2*r5;
+            fb[j]=a1*b2+a2*b1;
+        }
+        fft(fa,sz);fft(fb,sz);
+        vector<int> res(need);
+        for(int i=0;i<need;i++)
+        {
+            ll aa=fa[i].x+0.5;
+            ll bb=fb[i].x+0.5;
+            ll cc=fa[i].y+0.5;
+            res[i]=(aa+((bb%m)<<15)+((cc%m)<<30))%m;
+        }
+        return res;
+    }
+    vector<int> square(vector<int> &a,int m)
+    {
+        return multiply(a,a,m,1);
+    }
+};
+using namespace ntt;
+
+vi c[N];
+vi yo(int l,int r)
+{
+    if(l>r) return vi({1});
+    if(l==r) return c[l];
+    int mid=(l+r)/2;
+    vi a=yo(l,mid);
+    vi b=yo(mid+1,r);
+    return multiply(a,b,mod);
+}
+int a[N],b[N];
+int32_t main()
+{
+    ///BeatMeScanf;
+    int n=in(),k=in();
+    for(int i=1;i<=n;i++) a[i]=in();
+    int q=in();
+    while(q--){
+        int ty=in();
+        for(int i=1;i<=n;i++) b[i]=a[i];
+        int hudai=in();
+        if(ty==1){
+            int x=in(),d=in();
+            b[x]=d;
+        }
+        else{
+            int l=in(),r=in(),d=in();
+            for(int i=l;i<=r;i++) b[i]=(b[i]+d)%mod;
+        }
+        for(int i=1;i<=n;i++) c[i]=vi({(hudai-b[i]+mod)%mod,1});
+        vi ans=yo(1,n);
+        pf1(ans[n-k]);
+    }
+    return 0;
+}
+///Before submit=>
+///    *check for integer overflow,array bounds
+///    *check for n=1
+
