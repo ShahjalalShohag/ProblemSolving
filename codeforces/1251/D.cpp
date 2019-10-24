d7f3566d58d80213a367125ed28c5907f48cc86d
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2019-10-24 22:21:00 +0600

    Add solution for problem `D - Salary Changing` Link: https://codeforces.com/contest/1251/problem/D Tags: binary search, greedy, sortings, *1900 Ref: https://codeforces.com/contest/1251/submission/63328424

---
+++ codeforces/1251/D.cpp
@@ -0,0 +1,106 @@
+//#pragma comment(linker, "/stack:200000000")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma GCC optimize("unroll-loops")
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define vpii vector<pii>
+#define pb push_back
+#define eb emplace_back
+#define mt make_tuple
+#define MP make_pair
+#define fs first
+#define sc second
+#define F first
+#define S second
+#define nl "\n"
+#define asche cerr<<"Ekhane asche\n";
+#define pf printf
+#define sf scanf
+#define mem(a,x) memset(a,x,sizeof(a))
+#define SZ(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+#define one(x) __builtin_popcount(x)
+#define fout(x) fixed<<setprecision(x)
+#define BeatMeScanf ios_base::sync_with_stdio(false)
+//int dx[]={1,0,-1,0,1,-1,-1,1};
+//int dy[]={0,1,0,-1,1,1,-1,-1};
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+#define debug(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); deb(_it, args); }
+void deb(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void deb(istream_iterator<string> it, T a, Args... args) {
+    cerr << *it << " = " << a << endl;
+    deb(++it, args...);
+}
+//template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+namespace IO
+{
+    #define in() ( { int a ; scanf("%d",&a); a; } )
+    #define LL() ( { ll a ; scanf("%lld",&a); a; } )
+       #define ULL() ( { ull a ; scanf("%llu",&a); a; } )
+    #define DD() ({ double a; scanf("%lf", &a); a;})
+    #define CC() ( { char a ; scanf("%c",&a); a; } )
+    #define pf1(a) printf("%d\n",a)
+    #define pf1ll(a) printf("%lld\n",a)
+}
+using namespace IO;
+
+const int mod=1e9+7;
+const int N=3e5+9;
+const double eps=1e-9;
+const double PI=acos(-1.0);
+ll qpow(ll n,ll k) {ll ans=1;assert(k>=0);n%=mod;while(k>0){if(k&1) ans=(ans*n)%mod;n=(n*n)%mod;k>>=1;}return ans%mod;}
+
+int L[N],R[N];
+int32_t main()
+{
+    int t=in();
+    while(t--){
+        int n=in();
+        ll sum=LL();
+        for(int i=0;i<n;i++) L[i]=in(),R[i]=in();
+        int  l=0,r=1e9,ans=0;
+        while(l<=r){
+            int  mid=(l+r)/2;
+            ll  nw=0;
+            int choto=0,boro=0;
+            vi v;
+            for(int i=0;i<n;i++){
+                if(R[i]<mid) choto++,nw+=L[i];
+                else if(L[i]>mid) boro++,nw+=L[i];
+                else v.eb(L[i]);
+            }
+            if(2*choto>n) r=mid-1;
+            else if(2*boro>n) l=mid+1;
+            else{
+                sort(all(v));
+                for(int i=0;i<n/2-choto;i++) nw+=v[i];
+                for(int i=0;i<=n/2-boro;i++) nw+=mid;
+                if(nw<=sum){
+                    ans=mid;
+                    l=mid+1;
+                }
+                else r=mid-1;
+            }
+        }
+        pf1(ans);
+    }
+    return 0;
+}
+///Before submit=>
+///    *check for integer overflow,array bounds
+///    *check for n=1
+
