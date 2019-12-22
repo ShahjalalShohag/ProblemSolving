2a191ee3b1211ab75ab2337b2bdec42a848c6ef7
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2019-12-23 05:05:00 +0600

    Add solution for problem `G - Mass Change Queries` Link: https://codeforces.com/contest/911/problem/G Tags: data structures, *2500 Ref: https://codeforces.com/contest/911/submission/67446851

--- codeforces/911/G.cpp
+++ codeforces/911/G.cpp
@@ -1,138 +1,41 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
 #include<bits/stdc++.h>
 using namespace std;
-mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
 
-const int N = 3e5 + 9;
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define eb emplace_back
+#define ll long long
+#define nl '\n'
+#define deb(x) cerr<<#x" = "<<x<<nl
+#define in() ( { int a ; scanf("%d",&a); a; } )
 
-struct node {
-	node *l, *r;
-	int pos, key, sz = 0;
-	node(int position) {
-		l = r = nullptr;
-		pos = position;
-		key = rnd();
-	}
-	void pull()
-	{
-	    sz = 1;
-	    if(l) sz += l -> sz;
-	    if(r) sz += r -> sz;
-	}
-};
-struct treap
-{
-	node *root;
-	treap() {
-		root = nullptr;
-	}
-	void split(node *t, int pos, node *&l, node *&r)
-	{
-		if (t == nullptr) {
-			l = r = nullptr;
-			return;
-		}
-		if (t->pos < pos) {
-			split(t->r, pos, l, r);
-			t->r = l;
-			l = t;
-		}
-		else {
-			split(t->l, pos, l, r);
-			t->l = r;
-			r = t;
-		}
-		t -> pull();
-	}
-	node* merge(node *l, node *r)
-	{
-		if (!l || !r) return l ? l : r;
-		if (l->key < r->key) {
-			l->r = merge(l->r, r);
-			l -> pull();
-			return l;
-		}
-		r->l = merge(l, r->l);
-		r -> pull();
-		return r;
-	}
-	node* merge_op(node *l, node *r)
-    {
-        if(!l) { return r;}
-        if(!r) { return l;}
-
-        if(l->key < r->key) swap(l, r);
-
-        node *L, *R;
-        split(r, l->pos, L, R);
-        l->r = merge_op(l->r, R);
-        l->l = merge_op(L, l->l);
-        l -> pull();
-        return l;
-    }
-	void insert(int pos)
-	{
-		node *l, *r;
-        split(root, pos, l, r);
-        root = merge(merge(l, new node(pos)), r);
-	}
-	node* erase(int L, int R)
-	{
-        node *l, *r, *mid, *mr;
-        split(root, L, l, r);
-        split(r, R + 1, mid, mr);
-        root = merge(l, mr);
-        return mid;
-	}
-	void cmb(node* x)
-	{
-        if(!x) return;
-        if(x -> pos) insert(x -> pos);
-        cmb(x -> l);
-        cmb(x -> r);
-	}
-	void combine(node *x)
-	{
-        //root = merge(root, x);
-//        if(!x) return;
-//        if(!root || x -> sz > root -> sz) swap(x, root);
-//        cmb(x);
-        root = merge_op(root, x);
-	}
-	vector<int> ans;
-	void dfs(node* cur)
-	{
-	    if(!cur) return;
-	    ans.push_back(cur -> pos);
-        dfs(cur -> l);
-        dfs(cur -> r);
-	}
-	vector<int> get()
-	{
-	    ans.clear();
-	    dfs(root);
-	    return ans;
-	}
-}t[N];
-int ans[N];
+const int N = 2e5 + 9;
+const int mod = 1e9 + 7;
+unsigned char a[N];
 int32_t main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n; cin >> n;
-    for(int i = 1; i <= n; i++){
-        int k; cin >> k; t[k].insert(i);
-    }
-    int q; cin >> q;
+    int n=in();
+    for(int i=1; i<=n; i++) scanf("%hhu", &a[i]);
+    int q=in();
     while(q--){
-        int l, r, x, y; cin >> l >> r >> x >> y;
-        node* p = t[x].erase(l, r);
-        t[y].combine(p);
+        int l=in(), r=in();
+        unsigned char x, y; scanf("%hhu%hhu", &x, &y);
+        #define DO(i) a[i]=a[i]==x?y:a[i];
+        int i;
+        r-=31;
+        for(int i=l; i<=r; i+=32){
+            #define DO4(i) DO(i) DO(i+1) DO(i+2) DO(i+3)
+            DO4(i); DO4(i+4); DO4(i+8); DO4(i+12); DO4(i+16);
+            DO4(i+20); DO4(i+24); DO4(i+28);
+        }
+        r+=31;
+        for(int i=r-(r-l+1)%32+1; i<=r; i++) DO(i)
     }
-    for(int i = 1; i < N; i++){
-        auto p = t[i].get();
-        for(auto x: p) ans[x] = i;
-    }
-    for(int i = 1; i <= n; i++) cout << ans[i] << ' ';
+    for(int i=1; i<=n; i++) printf("%d ", a[i]);
+    puts("");
     return 0;
 }
