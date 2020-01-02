a6ff6fa07a2c883ef858084764a1192b67253ae3
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-01-03 01:43:00 +0600

    Add solution for problem `G - Sum the Fibonacci` Link: https://codeforces.com/contest/914/problem/G Tags: bitmasks, divide and conquer, dp, fft, math, *2600 Ref: https://codeforces.com/contest/914/submission/68094222

--- codeforces/914/G.cpp
+++ codeforces/914/G.cpp
@@ -1,4 +1,3 @@
-#pragma comment(linker, "/stack:200000000")
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
