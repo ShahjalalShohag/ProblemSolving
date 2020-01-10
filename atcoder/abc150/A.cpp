25faa87f4622d79303036d8066c44ebda8c467b4
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-01-10 18:37:00 +0600

    Add solution for problem `A - 500 Yen Coins` Link: https://atcoder.jp/contests/abc150/tasks/abc150_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc150/submissions/9395448

---
+++ atcoder/abc150/A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define eb emplace_back
+#define ll long long
+#define nl '\n'
+#define deb(x) cerr<<#x" = "<<x<<nl
+#define in() ( { int a ; scanf("%d",&a); a; } )
+
+const int N = 3e5 + 9;
+const int mod = 1e9 + 7;
+
+
+int32_t main()
+{
+    int n=in(), k=in();
+    if(n*500>=k) cout<<"Yes\n";
+    else cout<<"No\n";
+    return 0;
+}
