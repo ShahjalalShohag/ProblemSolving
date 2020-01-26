b8157b14603e60416608b91964a47d171c331403
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-01-26 18:12:00 +0600

    Add solution for problem `A - Serval vs Monster` Link: https://atcoder.jp/contests/abc153/tasks/abc153_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc153/submissions/9744058

---
+++ atcoder/abc153/A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define eb emplace_back
+#define ll long long
+#define nl '\n'
+#define deb(x) cerr<<#x" = "<<x<<nl
+#define in() ( { int a ; scanf("%d",&a); a; } )
+
+const int N = 3e5 + 9;
+const int mod = 1e9 + 7;
+
+
+int32_t main()
+{
+    int h = in(), a = in();
+    cout << (h+a-1)/a << nl;
+    return 0;
+}
