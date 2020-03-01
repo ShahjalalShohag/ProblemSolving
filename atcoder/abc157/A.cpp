af2613ee2d22abce7f123302456bcef85ae97b05
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-03-01 18:10:00 +0600

    Add solution for problem `A - Duplex Printing` Link: https://atcoder.jp/contests/abc157/tasks/abc157_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc157/submissions/10434373

---
+++ atcoder/abc157/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define deb(x) cerr<<#x" = "<<x<<nl
+#define in() ( { int a ; scanf("%d", &a); a; } )
+
+const int N = 3e5 + 9;
+const int mod = 1e9 + 7;
+
+int32_t main()
+{
+    int n; cin >> n; cout << (n + 1) / 2 << nl;
+    return 0;
+}
