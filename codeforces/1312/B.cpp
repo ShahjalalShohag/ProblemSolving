01b4f9933efb4b2e032c8c63f6e0337c7698fed7
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-03-09 20:46:00 +0600

    Add solution for problem `B - Bogosort` Link: https://codeforces.com/contest/1312/problem/B Tags: constructive algorithms, sortings, *1000 Ref: https://codeforces.com/contest/1312/submission/72803403

---
+++ codeforces/1312/B.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define deb(x) cerr<<#x" = "<<x<<nl
+#define in() ( { int a ; scanf("%d", &a); a; } )
+
+const int N = 3e5 + 9;
+const int mod = 1e9 + 7;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        vector<int> a(n);
+        for(int i = 0; i < n; i++) cin >> a[i];
+        sort(a.begin(), a.end());
+        reverse(a.begin(), a.end());
+        for(auto x: a) cout << x << ' ';
+        cout << nl;
+    }
+    return 0;
+}
