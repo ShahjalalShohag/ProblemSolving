f9cc0d3b2c4b8b63eb54ba942d7e0697ea405742
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-03-28 18:01:00 +0600

    Add solution for problem `A - Coffee` Link: https://atcoder.jp/contests/abc160/tasks/abc160_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc160/submissions/11265874

---
+++ atcoder/abc160/A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3e5 + 9;
+
+int32_t main()
+{
+    string s; cin >> s;
+    if(s[2] == s[3] && s[4] == s[5]) cout << "Yes\n"; else cout << "No\n";
+    return 0;
+}
