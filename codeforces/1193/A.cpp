5b3093fc1091de7428839572910c2350ddd8c75d
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-04-01 16:02:00 +0600

    Add solution for problem `A - Amusement Park` Link: https://codeforces.com/contest/1193/problem/A Tags: *special problem, dp, math Ref: https://codeforces.com/contest/1193/submission/75065171

--- codeforces/1193/A.cpp
+++ codeforces/1193/A.cpp
@@ -35,128 +35,28 @@ template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { re
 
 using mint = modint<mod>;
 
-//https://codeforces.com/contest/1193/submission/57949255
-const int V = 18;
-struct ChromaticPolynomial
-{
-    int n;
-    mint nCr(int n, int k)
-    {
-        if(n < 0) return 0;
-        if(n - k < k) k = n - k;
-        if(k < 0) return 0;
-        mint ret = 1;
-        for(int i = 0; i < k; ++i) ret *= n-i, ret /= i+1;
-        return ret;
-    }
-
-    void bitflip(vector<mint> &v)
-    {
-        for(int i = 1, j = 0; i < n; ++i){
-            int m = n >> 1;
-            for(; j >= m; m >>= 1) j -= m;
-            j += m;
-            if(i < j) swap(v[i], v[j]);
-        }
-    }
-    // fast mobius transform.
-    // turns or convolution into point-wise multiplication
-    void fmt(vector<mint> &vec, bool inv)
-    {
-        int n = vec.size();
-        for(int iter = 1, sh = 0; iter < n; iter *= 2, ++sh){
-            for(int x = 0; x < n; x += 2 * iter){
-                for(int y = 0; y < iter; ++y){
-                    mint v = vec[x + y], w = vec[x + y + iter];
-                    vec[x + y + iter] = inv ? w - v : v + w;
-                }
-            }
-        }
-    }
-    // O(n^3 2^n)
-    // Let P(x) = chromatic polynomial of g. it returns {P(0), P(1), ... P(n)}
-    vector<mint> polynomial(vector<bitset<V>> g)
-    {
-        if(!n){
-            vector<mint> ans(n + 1);
-            fill(ans.begin(), ans.end(), 1);
-            return ans;
-        }
-        vector<vector<mint>> f(n + 1, vector<mint>(1 << n));
-        for(int m = 1; m < (1 << n); ++m){
-            bitset<V> bs(0);
-            bool fail = 0;
-            for(int i = 0; i < n; ++i){
-                if((m >> i)&1){
-                    if(bs[i]) fail = 1;
-                    bs |= g[i];
-                }
-            }
-            if(!fail) f[__builtin_popcount(m)][m] = 1;
-        }
-
-        for(auto &e:f) fmt(e, 0);
-        vector<vector<mint>> f_cur(n + 1, vector<mint>(1 << n));
-        auto f_next = f_cur;
-        f_cur[0][0] = 1;
-        fmt(f_cur[0], 0);
-        vector<mint> ans(n + 1, 0);
-        vector<mint> tmp(1 << n);
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j <= n; ++j){
-                auto &e = f_next[j];
-                fill(tmp.begin(), tmp.end(), 0);
-                for(int k = 0; k <= j; ++k){
-                    for(int l = 0; l < (1 << n); ++l) tmp[l] += f_cur[j-k][l] * f[k][l];
-                }
-                for(int l = 0; l < (1 << n); ++l) e[l] = tmp[l];
-            }
-            f_next.swap(f_cur);
-            auto cnts = f_cur[n];
-            fmt(cnts, 1);
-            for(int j = 0; j <= n; ++j) ans[j] += nCr(j, i + 1) * cnts.back();
-        }
-        return ans;
-    }
-    vector<mint> p;
-    void init(vector<bitset<V>> g)
-    {
-        n = g.size();
-        p = polynomial(g);
-    }
-    mint get(long long c)
-    {
-        if(c >= 0 && c < (int)p.size()) return p[c];
-        c = (c % mod + mod) % mod;
-        // polynomial interpolation
-        mint ans = 0;
-        for(int i = 0; i <= n; ++i){
-            mint mul = p[i];
-            for(int j = 0; j <= n; ++j) if(j != i){
-                mul *= mint(c) - mint(j);
-                mul /= mint(i) - mint(j);
-            }
-            ans += mul;
-        }
-        return ans;
-    }
-} C;
-
-vector<int> g[N];
+mint dp[1 << 18];
+int e[1 << 18];
 int32_t main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
     int n, m; cin >> n >> m;
     for(int i = 1; i <= m; i++){
-        int u, v; cin >> u >> v; --u; --v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        int u, v; cin >> u >> v; --u, --v;
+        e[1 << u] |= 1 << v;
+        e[1 << v] |= 1 << u;
     }
-    vector<bitset<V>> e(n);
-    for(int u = 0; u < n; u++) for(auto v: g[u]) e[u][v] = 1;
-    C.init(e);
-    mint ans = C.get(-1);
-    ans *= n & 1? -1 : 1;
-    ans *= m; ans /= 2;
-    cout << ans << '\n';
+    dp[0] = 1;
+    for(int mask = 1; mask < (1 << n); mask++){
+        for(int i = 0; i < n; i++) if((mask >> i) & 1) e[mask] |= e[1 << i];
+        for(int sub = mask; sub; sub = (sub - 1) & mask){
+            if(e[sub] & sub) continue;
+            if(__builtin_popcount(sub) & 1) dp[mask] += dp[mask ^ sub];
+            else dp[mask] -= dp[mask ^ sub];
+        }
+    }
+    cout << dp[(1 << n) - 1] * m / 2 << '\n';
     return 0;
 }
