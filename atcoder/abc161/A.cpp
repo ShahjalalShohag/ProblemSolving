8f4dea95940204b6861ff5fd44b6c2d0c744d2af
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-04-04 18:01:00 +0600

    Add solution for problem `A - ABC Swap` Link: https://atcoder.jp/contests/abc161/tasks/abc161_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc161/submissions/11490220

---
+++ atcoder/abc161/A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3e5 + 9;
+
+int32_t main()
+{
+    int a, b, c; cin >> a >> b >> c;
+    swap(a, b); swap(a, c);
+    cout << a << ' ' << b << ' ' << c << endl;
+    return 0;
+}
