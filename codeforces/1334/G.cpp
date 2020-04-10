069375f59df8f25afdf00ea076358725716dad88
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-04-10 22:59:00 +0600

    Add solution for problem `G - Substring Search` Link: https://codeforces.com/contest/1334/problem/G Tags: bitmasks, brute force, fft, *2900 Ref: https://codeforces.com/contest/1334/submission/76192293

--- codeforces/1334/G.cpp
+++ codeforces/1334/G.cpp
@@ -1,5 +1,5 @@
 #pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma,tune=native")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
 
 #include<bits/stdc++.h>
