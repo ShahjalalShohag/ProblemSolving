3ba8b9b6b3382e307f74ffd9db5f16851df9df17
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-04-10 23:04:00 +0600

    Add solution for problem `G - Substring Search` Link: https://codeforces.com/contest/1334/problem/G Tags: bitmasks, brute force, fft, *2900 Ref: https://codeforces.com/contest/1334/submission/76192985

---
+++ codeforces/1334/G.cpp
@@ -0,0 +1,19 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 2e5 + 1;
+bitset<N> bs[26], ans; int p[26];
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    for(int i = 0; i < 26; i++) cin >> p[i], p[i]--;
+    string s, t; cin >> s >> t;
+    for(int i = 0; t[i]; i++) bs[t[i] - 'a'][i] = 1;
+    ans.set();
+    for(int i = 0; s[i]; i++) ans &= (bs[s[i] - 'a'] | bs[p[s[i] - 'a']]) >> i;
+    for(int i = 0; i <= t.size() - s.size(); i++) cout << ans[i];
+    return 0;
+}
