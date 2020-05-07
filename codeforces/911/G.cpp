7547e93aa25cab4c439b888a5c26a94fae7ce68d
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-05-07 17:49:00 +0600

    Add solution for problem `G - Mass Change Queries` Link: https://codeforces.com/contest/911/problem/G Tags: data structures, *2500 Ref: https://codeforces.com/contest/911/submission/79265222

--- codeforces/911/G.cpp
+++ codeforces/911/G.cpp
@@ -1,59 +1,99 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
-#pragma GCC optimize("unroll-loops")
-
-#include <string>
-#include <bits/functexcept.h>
-#include <iosfwd>
-#include <bits/cxxabi_forced.h>
-#include <bits/functional_hash.h>
-
-#pragma push_macro("__SIZEOF_LONG__")
-#pragma push_macro("__cplusplus")
-#define __SIZEOF_LONG__ __SIZEOF_LONG_LONG__
-#define unsigned unsigned long
-#define __cplusplus 201102L
-
-#define __builtin_popcountl __builtin_popcountll
-#define __builtin_ctzl __builtin_ctzll
-
-#include <bitset>
-
-#pragma pop_macro("__cplusplus")
-#pragma pop_macro("__SIZEOF_LONG__")
-#undef unsigned
-#undef __builtin_popcountl
-#undef __builtin_ctzl
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+
 const int N = 2e5 + 9;
 
-int a[N], ans[N];
-bitset<N> bs[101], nw, t;
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+struct node {
+    node *l, *r;
+    int pos, key;
+    node(int id) {
+        l = r = nullptr;
+        pos = id; key = rnd();
+    }
+};
+struct treap {
+    node *root;
+    treap() {
+        root = nullptr;
+    }
+    void split(node *t, int pos, node *&l, node *&r) {
+        if (t == nullptr) {
+            l = r = nullptr;
+            return;
+        }
+        if (t->pos <= pos) {
+            split(t->r, pos, l, r);
+            t->r = l; l = t;
+        } 
+        else {
+            split(t->l, pos, l, r);
+            t->l = r; r = t;
+        }
+    }
+    node* merge(node *l, node *r) {
+        if (!l || !r) return l ? l : r;
+        if (l->key < r->key) {
+            l->r = merge(l->r, r);
+            return l;
+        }
+        r->l = merge(l, r->l);
+        return r;
+    }
+    node* merge_treap(node *l, node *r) {
+        if(!l) return r;
+        if(!r) return l;
+        if(l->key < r->key) swap(l, r);
+        node *L, *R;
+        split(r, l->pos, L, R);
+        l->r = merge_treap(l->r, R);
+        l->l = merge_treap(L, l->l);
+        return l;
+    }
+    void insert(int id) {
+        node *l, *r;
+        split(root, id, l, r);
+        root = merge(merge(l, new node(id)), r);
+    }
+    node* cut(int L, int R) {
+        node *l, *r, *mid, *mr;
+        split(root, L - 1, l, r);
+        split(r, R, mid, mr);
+        root = merge(l, mr);
+        return mid;
+    }
+    void combine(node *x) { //combine with another treap
+        root = merge_treap(root, x);
+    }
+    vector<int> ans;
+    void dfs(node* cur) {
+        if(!cur) return;
+        ans.push_back(cur -> pos);
+        dfs(cur -> l); dfs(cur -> r);
+    }
+    vector<int> get() {
+        ans.clear(); dfs(root);
+        return ans;
+    }
+} t[N];
+int ans[N];
 int32_t main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int n; cin >> n;
-    for(int i = 1; i <= n; i++){
-        int k; cin >> k; bs[k][i] = true;
+    for(int i = 1; i <= n; i++) {
+        int k; cin >> k;
+        t[k].insert(i);
     }
     int q; cin >> q;
-    while(q--){
+    while(q--) {
         int l, r, x, y; cin >> l >> r >> x >> y;
-        if(x == y) continue;
-        nw.set();
-        nw <<= N - (r - l + 1);
-        nw >>= N - r - 1;
-        t = bs[x] & nw;
-        bs[x] &= ~t;
-        bs[y] |= t;
+        node *p = t[x].cut(l, r);
+        t[y].combine(p);
     }
-    for(int i = 1; i <= 100; i++){
-        int p = bs[i]._Find_first();
-        while(p < N){
-            ans[p] = i;
-            p = bs[i]._Find_next(p);
-        }
+    for(int i = 1; i < N; i++) {
+        auto p = t[i].get();
+        for(auto x: p) ans[x] = i;
     }
     for(int i = 1; i <= n; i++) cout << ans[i] << " \n"[i == n];
     return 0;
