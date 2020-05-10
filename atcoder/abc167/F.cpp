5dea21bcdf5203b4f687efc346f63475dbc59c11
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-05-11 00:54:00 +0600

    Add solution for problem `F - Bracket Sequencing` Link: https://atcoder.jp/contests/abc167/tasks/abc167_f Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/abc167/submissions/13103853

--- atcoder/abc167/F.cpp
+++ atcoder/abc167/F.cpp
@@ -1,39 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 const int N = 3e5 + 9;
- 
+
 int32_t main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	int n; cin >> n; int tot = 0;
-	vector<array<int, 3>> pos, neg;
+	vector<array<int, 3>> a, last; vector<int> ans;
+	int cur = 0;
 	for (int i = 1; i <= n; i++) {
 		string s; cin >> s;
-		int mn = 0, cur = 0;
+		vector<char> v;
 		for (auto x: s) {
-			if (x == '(') cur++;
-			else cur--;
-			mn = min(mn, cur);
+			if (x == '(') tot++, v.push_back('(');
+			else {
+				tot--;
+				if (v.size() && v.back() == '(') v.pop_back();
+				else v.push_back(')');
+			}
 		}
-		if (cur >= 0) pos.push_back({-mn, -cur, i});
-		else neg.push_back({mn - cur, -cur, i});
-		tot += cur;
+		int op = 0, cl = 0;
+		for (auto x: v) {
+			op += x == '(';
+			cl += x == ')';
+		}
+		if (op && !cl) ans.push_back(i), cur += op;
+		else if (cl && !op) last.push_back({cl, 0, i});
+		else if (op && cl) a.push_back({op - cl, op, i});
+		else ans.push_back(i);
 	}
 	if (tot != 0) return cout << "No\n", 0;
-	sort(pos.begin(), pos.end());
-	int cur = 0; vector<int> ans;
-	for (auto x: pos) {
-		if (x[0] > cur) return cout << "No\n", 0; 
-		cur -= x[1];
+	sort(a.begin(), a.end());
+	reverse(a.begin(), a.end());
+	for (auto x: a) {
+		if (cur < x[1] - x[0]) return cout << "No\n", 0;
+		cur += x[0];
 		ans.push_back(x[2]);
 	}
-	sort(neg.begin(), neg.end());
-	for (auto x: neg) {
-		if (x[1] - x[0] > cur) return cout << "No\n", 0;
-		cur -= x[1];
+	for (auto x: last) {
+		if (cur < x[0]) return cout << "No\n", 0;
+		cur -= x[0];
 		ans.push_back(x[2]);
 	}
 	cout << "Yes\n";
+	//for (auto x: ans) cout << x << ' '; cout << '\n';
     return 0;
 }
