16d8d7f7000135ecc8286d29bf0746170b2d940f
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-06-07 16:32:00 +0600

    Add solution for problem `G - Design Tutorial: Increase the Constraints` Link: https://codeforces.com/contest/472/problem/G Tags: bitmasks, data structures, fft, *2800 Ref: https://codeforces.com/contest/472/submission/82770701

--- codeforces/472/G.cpp
+++ codeforces/472/G.cpp
@@ -2,32 +2,22 @@
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
 
+
 #include<bits/stdc++.h>
 using namespace std;
 
 const int N = 2e5 + 9;
 
-inline int popcount(int x){
-    int counter = 0;
-    __asm__ volatile ("POPCNT %1, %0;"
-        :"=r"(counter)
-        :"r"(x)
-        :
-    );
-    return counter;
-}
 unsigned long long a[N], b[N];
 int32_t main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	string A, B; cin >> A >> B;
-	int M = 0;
-	for (int i = 0; i < 31; i++) M |= 1 << i;
 	int n = A.size();
 	for (int i = 0; i < n; i++) {
-		if (i + 61 < n) {
+		if (i + 63 < n) {
 			unsigned long long mask = 0;
-			for (int j = i; j <= i + 61; j++) {
+			for (int j = i; j <= i + 63; j++) {
 				mask = (mask << 1) + (A[j] - '0');
 			}
 			a[i] = mask;
@@ -35,9 +25,9 @@ int32_t main() {
 	}
 	int m = B.size();
 	for (int i = 0; i < m; i++) {
-		if (i + 61 < m) {
+		if (i + 63 < m) {
 			unsigned long long mask = 0;
-			for (int j = i; j <= i + 61; j++) {
+			for (int j = i; j <= i + 63; j++) {
 				mask = (mask << 1) + (B[j] - '0');
 			}
 			b[i] = mask;
@@ -47,10 +37,9 @@ int32_t main() {
 	while (q--) {
 		int l1, l2, x; cin >> l1 >> l2 >> x;
 		int ans = 0;
-		while (x >= 62) {
-			unsigned long long nw = a[l1] ^ b[l2];
-			ans += popcount(nw & M) + popcount(nw >> 31);
-			l1 += 62; l2 += 62; x -= 62;
+		while (x >= 64) {
+			ans += __builtin_popcountll(a[l1] ^ b[l2]);
+			l1 += 64; l2 += 64; x -= 64;
 		}
 		for (int i = 0; i < x; i++) ans += A[l1 + i] != B[l2 + i];
 		cout << ans << '\n';
