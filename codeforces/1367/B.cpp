8ee8aaa09fa423b3fabeab3d7844cf1a5ce9e985
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-06-16 20:42:00 +0600

    Add solution for problem `B - Even Array` Link: https://codeforces.com/contest/1367/problem/B Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1367/submission/83941511

--- codeforces/1367/B.cpp
+++ codeforces/1367/B.cpp
@@ -1,27 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int N = 3e5 + 9;
+
 int32_t main() {
-   ios_base::sync_with_stdio(0);
-   cin.tie(0);
-   int t; cin >> t;
-   while (t--) {
-   	int n; cin >> n;
-   	vector<int> cnt(2, 0);
-   	int tot = 0;
-   	for (int i = 0; i < n; i++) {
-   		int k; cin >> k;
-   		if (k % 2 != i % 2) {
-   			tot++;
-   		}
-   		cnt[k % 2]++;
-   	}
-   	if (cnt[1] == n / 2) {
-   		cout << (tot + 1) / 2 << '\n';
-   	}
-   	else {
-   		cout << -1 << '\n';
-   	}
-   }
-   return 0;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t; cin >> t;
+	while (t--) {
+		int n; cin >> n;
+		vector<int> cnt(2, 0);
+		int tot = 0;
+		for (int i = 0; i < n; i++) {
+			int k; cin >> k;
+			cnt[k % 2]++;
+			tot += k % 2 != i % 2;
+		}
+		if (cnt[1] != n / 2) cout << - 1 << '\n';
+		else cout << (tot + 1) / 2 << '\n';
+	}
+    return 0;
 }
