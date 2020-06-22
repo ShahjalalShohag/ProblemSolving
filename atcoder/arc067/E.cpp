bdf82c025b12143309fa50b2416c504c4436d8e6
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-06-22 06:32:00 +0600

    Add solution for problem `E - Grouping` Link: https://atcoder.jp/contests/arc067/tasks/arc067_c Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/arc067/submissions/14601096

---
+++ atcoder/arc067/E.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1010, mod = 1e9 + 7;
+
+template <const int32_t MOD>
+struct modint {
+    int32_t value;
+    modint() = default;
+    modint(int32_t value_) : value(value_) {}
+    inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+    inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+    inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+    modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; }
+    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (modint<MOD> other) const { return value == other.value; }
+    inline bool operator != (modint<MOD> other) const { return value != other.value; }
+    inline bool operator < (modint<MOD> other) const { return value < other.value; }
+    inline bool operator > (modint<MOD> other) const { return value > other.value; }
+};
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> istream & operator >> (istream & in, modint<MOD> &n) { return in >> n.value; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
+
+using mint = modint<mod>;
+
+struct Combi{
+	int n; vector<mint> facts, finvs, invs;
+	Combi(int _n): n(_n), facts(_n), finvs(_n), invs(_n){
+		facts[0] = finvs[0] = 1;
+		invs[1] = 1;
+	    for (int i = 2; i < n; i++) invs[i] =  invs[mod % i] * (-mod / i);
+		for(int i = 1; i < n; i++){
+			facts[i] = facts[i - 1] * i;
+			finvs[i] = finvs[i - 1] * invs[i];
+		}
+	}
+	inline mint fact(int n) { return facts[n]; }
+	inline mint finv(int n) { return finvs[n]; }
+	inline mint inv(int n) { return invs[n]; }
+	inline mint ncr(int n, int k) { return n < k ? 0 : facts[n] * finvs[k] * finvs[n-k]; }
+};
+Combi C(N);
+
+mint dp[N][N];
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n, a, b, c, d; cin >> n >> a >> b >> c >> d;
+	dp[a - 1][0] = 1;
+	for (int g = a; g <= b; g++) {
+		for (int i = 0; i <= n; i++) {
+			int cur = i;
+			mint mul = 1;
+			for (int k = 0; k * g <= i; k++) {
+				if (k == 0 || k >= c && k <= d) dp[g][i] += mul * dp[g - 1][cur] / C.fact(k);
+				mul *= C.ncr(cur, g);
+				cur -= g;
+			}
+		}
+	}
+	cout << dp[b][n] << '\n';
+    return 0;
+}
