4632c8ca99df389cb2e168d613f586ebc6626a76
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-08-19 15:39:00 +0600

    Add solution for problem `F - Independent Set` Link: https://codeforces.com/contest/1332/problem/F Tags: dfs and similar, dp, trees, *2500 Ref: https://codeforces.com/contest/1332/submission/90376214

---
+++ codeforces/1332/F.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3e5 + 9, mod = 998244353;
+
+template <const int32_t MOD>
+struct modint {
+    int32_t value;
+    modint() = default;
+    modint(int32_t value_) : value(value_) {}
+    inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+    inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+    inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+    modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; }
+    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (modint<MOD> other) const { return value == other.value; }
+    inline bool operator != (modint<MOD> other) const { return value != other.value; }
+    inline bool operator < (modint<MOD> other) const { return value < other.value; }
+    inline bool operator > (modint<MOD> other) const { return value > other.value; }
+};
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> istream & operator >> (istream & in, modint<MOD> &n) { return in >> n.value; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
+
+using mint = modint<mod>;
+
+vector<int> g[N];
+mint dp[N][3];
+
+void dfs(int u, int p) {
+	dp[u][0] = dp[u][1] = dp[u][2] = 1;
+	for (auto v: g[u]) {
+		if (v == p) continue;
+		dfs(v, u);
+		dp[u][0] *= dp[v][0] + dp[v][1] + dp[v][0] + dp[v][1] + dp[v][2];
+		dp[u][1] *= dp[v][0] + dp[v][0] + dp[v][1];
+		dp[u][2] *= dp[v][0] + dp[v][1];
+	}
+	dp[u][1] -= dp[u][2];
+}
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n; cin >> n;
+	for (int i = 1; i < n; i++) {
+	    int u, v; cin >> u >> v;
+	    g[u].push_back(v);
+	    g[v].push_back(u);
+	}
+	dfs(1, 0);
+	cout << dp[1][1] + dp[1][0] - 1<< '\n';
+    return 0;
+}
