8c750b470469bf05323b0f7c8c2ab07d1bac7ccf
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-09-14 05:28:00 +0600

    Add solution for problem `F - Xors on Segments` Link: https://codeforces.com/contest/620/problem/F Tags: data structures, strings, trees, *2800 Ref: https://codeforces.com/contest/620/submission/92753106

--- codeforces/620/F.cpp
+++ codeforces/620/F.cpp
@@ -7,17 +7,16 @@ using namespace std;
 
 const int N = 5e4 + 2;
 int a[N], ans[N], l[N], r[N], dp[N];
-int f[1 << 20], g[N];
+int f[1 << 20];
 int32_t main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	int n, q; cin >> n >> q;
-	for (int i = 1; i < (1 << 20); i++) {
-		f[i] = f[i - 1] ^ i;
-	}
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
-		g[i] = f[a[i]];
+	}
+	for (int i = 1; i < (1 << 20); i++) {
+		f[i] = f[i - 1] ^ i;
 	}
 	for (int i = 0; i < q; i++) {
 		cin >> l[i] >> r[i];
@@ -26,7 +25,7 @@ int32_t main() {
 	for (int i = 0; i < n; i++) {
 		dp[i] = a[i];
 		for (int j = i + 1; j < n; j++) {
-			int k = g[i] ^ g[j] ^ (a[i] < a[j] ? a[i] : a[j]);
+			int k = a[i] > a[j] ? f[a[i]] ^ f[a[j] - 1] : f[a[j]] ^ f[a[i] - 1];
 			dp[j] = dp[j - 1] > k ? dp[j - 1] : k;
 		}
 		for (int j = 0; j < q; j++) {
