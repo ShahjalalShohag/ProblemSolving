66ee9c26348e6425bb8bfc115a5a63fecc9b8c3b
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-09-15 19:28:00 +0600

    Add solution for problem `E - Expected Damage` Link: https://codeforces.com/contest/1418/problem/E Tags: binary search, combinatorics, probabilities, *2400 Ref: https://codeforces.com/contest/1418/submission/92905663

---
+++ codeforces/1418/E.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 2e5 + 9, mod = 998244353;
+
+template <const int32_t MOD>
+struct modint {
+    int32_t value;
+    modint() = default;
+    modint(int32_t value_) : value(value_) {}
+    inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+    inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+    inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+    modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; }
+    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (modint<MOD> other) const { return value == other.value; }
+    inline bool operator != (modint<MOD> other) const { return value != other.value; }
+    inline bool operator < (modint<MOD> other) const { return value < other.value; }
+    inline bool operator > (modint<MOD> other) const { return value > other.value; }
+};
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> istream & operator >> (istream & in, modint<MOD> &n) { return in >> n.value; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
+
+using mint = modint<mod>;
+
+mint suf[N];
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n, q; cin >> n >> q;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end());
+	for (int i = n - 1; i >= 0; i--) {
+		suf[i] = suf[i + 1] + a[i] % mod;
+	}
+	auto prob = [&](int k, int big, int small) {
+		if (k > big) return mint(0);
+		return mint(big - k + 1) / (big + 1);
+	};
+	while (q--) {
+		int k, s; cin >> k >> s;
+		int i = lower_bound(a.begin(), a.end(), s) - a.begin();
+		mint ans = suf[i] * prob(k, n - i - 1, i);
+		ans += (suf[0] - suf[i]) * prob(k, n - i, i - 1);
+		cout << ans << '\n';
+	}
+    return 0;
+}
