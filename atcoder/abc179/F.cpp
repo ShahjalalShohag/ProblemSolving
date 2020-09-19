3429572f27aee16b24b8fa3823f9e3f147579da7
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-09-19 18:46:00 +0600

    Add solution for problem `F - Simplified Reversi` Link: https://atcoder.jp/contests/abc179/tasks/abc179_f Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/abc179/submissions/16876137

--- atcoder/abc179/F.cpp
+++ atcoder/abc179/F.cpp
@@ -5,22 +5,29 @@ int32_t main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	int n, q; cin >> n >> q;
-	set<pair<int, int>> se[2];
-	se[0].insert({n, n});
-	se[1].insert({n, n});
+	set<pair<int, int>> r, c;
+	r.insert({n, n});
+	c.insert({n, n});
 	while (q--) {
 		int ty, i; cin >> ty >> i;
-		ty--;
-		auto it = se[ty].lower_bound({i, -1});
-		int k = (*it).second;
-		se[ty ^ 1].insert({(k - 1), i});
+		if (ty == 1) {
+			auto it = r.lower_bound({i, -1});
+			int k = (*it).second;
+			c.insert({(k - 1), i});
+		}
+		else {
+			auto it = c.lower_bound({i, -1});
+			int k = (*it).second;
+			r.insert({(k - 1), i});
+		}
 	}
 	long long ans = 1LL * (n - 2) * (n - 2);
-	for (int i = 0; i < 2; i++) {
-		for (auto x: se[i]) {
-			if (x.second != n) ans -= x.first - 1;
-		}
+	for (auto x: r) {
+		if (x.second != n) ans -= x.first - 1;
 	}	
+	for (auto x: c) {
+		if (x.second != n) ans -= x.first - 1;
+	}
 	cout << ans << '\n';
     return 0;
 }
