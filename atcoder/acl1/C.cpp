4252aefd627c765f2c9577fbb3f0e4671e0fae01
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-09-20 21:24:00 +0600

    Add solution for problem `C - Moving Pieces` Link: https://atcoder.jp/contests/acl1/tasks/acl1_c Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/acl1/submissions/16922680

--- atcoder/acl1/C.cpp
+++ atcoder/acl1/C.cpp
@@ -1,142 +1,113 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
-const int N = 51 * 51;
-// Complexity: O(n^3) but optimized
-// It finds minimum cost maximum matching.
-// For finding maximum cost maximum matching
-// add -cost and return -matching()
-// 1-indexed
-struct Hungarian {
-    int c[N][N], fx[N], fy[N], d[N];
-    int l[N], r[N], arg[N], trace[N];
-    queue<int> q;
-    int start, finish, n;
-    const int inf = 1e9;
-    Hungarian() {}
-    Hungarian(int n): n(n) {
-        for (int i = 1; i <= n; ++i) {
-            fy[i] = l[i] = r[i] = 0;
-            for (int j = 1; j <= n; ++j) c[i][j] = inf;
+
+const int N = 50 * 50 + 200;
+
+struct MCMF { //1 indexed
+    using T = int; static const T inf = 1e9 + 7;
+    int n, m, s, t; bool vis[N];
+    int par[N], pos[N]; T pot[N], dis[N], left[N];
+    priority_queue<pair<T, int>> q;
+    struct edge {
+        int to, rev; T cap, cost, flow; int id;
+    };
+    vector<edge> ed[N];
+    MCMF() {}
+    MCMF(int _n) {
+        n = _n + 2;
+        for (int i = 1; i <= n; i++) ed[i].clear();
+    }
+    void add_edge(int u, int v, T cap, T cost, int id = -1) {
+        edge a = {v, (int)ed[v].size(), cap, cost, 0, id};
+        edge b = {u, (int)ed[u].size(), 0, -cost, 0, -1};
+        ed[u].push_back(a); ed[v].push_back(b);
+    }
+    T BellmanFord(int u) {
+        if (vis[u]) return pot[u];
+        if (u == s) {
+            pot[s] = 0;
+            return 0;
         }
-    }
-    void add_edge(int u, int v, int cost) {
-        c[u][v] = min(c[u][v], cost);
-    }
-    inline int getC(int u, int v) {
-        return c[u][v] - fx[u] - fy[v];
-    }
-    void initBFS() {
-        while (!q.empty()) q.pop();
-        q.push(start);
-        for (int i = 0; i <= n; ++i) trace[i] = 0;
-        for (int v = 1; v <= n; ++v) {
-            d[v] = getC(start, v);
-            arg[v] = start;
+        vis[u] = true; pot[u] = inf;
+        for (edge e : ed[u]) {
+            edge r = ed[e.to][e.rev];
+            if (r.flow < r.cap) pot[u] = min(pot[u], BellmanFord(e.to) + r.cost);
         }
-        finish = 0;
+        return pot[u];
     }
-    void findAugPath() {
+    bool Dijkstra() {
+        memset(vis, 0, sizeof vis);
+        for (int i = 1; i <= n; i++) dis[i] = left[i] = inf;
+        dis[s] = 0; q.push({0, s}); int u, v;
         while (!q.empty()) {
-            int u = q.front(); q.pop();
-            for (int v = 1; v <= n; ++v) if (!trace[v]) {
-                int w = getC(u, v);
-                if (!w) {
-                    trace[v] = u;
-                    if (!r[v]) {
-                        finish = v;
-                        return;
-                    }
-                    q.push(r[v]);
+            u = q.top().second; q.pop();
+            if (vis[u]) continue;
+            vis[u] = true; int ptr = 0;
+            for(edge e : ed[u]) {
+                v = e.to; T cost = e.cost + pot[u] - pot[v];
+                if (e.flow < e.cap && dis[u] + cost < dis[v]) {
+                    dis[v] = dis[u] + cost;
+                    par[v] = u; pos[v] = ptr;
+                    left[v] = min(left[u], e.cap - e.flow);
+                    q.push(make_pair(-dis[v], v));
                 }
-                if (d[v] > w) {
-                    d[v] = w;
-                    arg[v] = u;
-                }
+                ++ptr;
             }
         }
+        for (int i = 1; i <= n; i++) dis[i] += (pot[i] - pot[s]);
+        return vis[t];
     }
-    void subX_addY() {
-        int delta = inf;
-        for (int v = 1; v <= n; ++v) if (trace[v] == 0 && d[v] < delta) {
-            delta = d[v];
-        }
-        // Rotate
-        fx[start] += delta;
-        for (int v = 1; v <= n; ++v) if(trace[v]) {
-            int u = r[v];
-            fy[v] -= delta;
-            fx[u] += delta;
-        } else d[v] -= delta;
-        for (int v = 1; v <= n; ++v) if (!trace[v] && !d[v]) {
-            trace[v] = arg[v];
-            if (!r[v]) {
-                finish = v; return;
+    pair<T, T> solve(int _s, int _t, T goal = inf) {
+        s = _s; t = _t;
+        memset(pot, 0, sizeof pot);
+        memset(vis, 0, sizeof vis);
+        BellmanFord(t);
+        int u, v; T ans = 0, cost = 0, f;
+        while (ans < goal && Dijkstra()) {
+            u = t; f = left[t];
+            while (u != s) {
+                v = par[u];
+                ed[v][pos[u]].flow += left[t];
+                ed[u][ed[v][pos[u]].rev].flow -= left[t];
+                u = v;
             }
-            q.push(r[v]);
+            T need = goal - ans; f = min(f, need);
+            ans += f; cost += f * dis[t];
+            memcpy(pot, dis, sizeof dis);
         }
-    }
-    void Enlarge() {
-        do {
-            int u = trace[finish];
-            int nxt = l[u];
-            l[u] = finish;
-            r[finish] = u;
-            finish = nxt;
-        } while (finish);
-    }
-    int maximum_matching() {
-        for (int u = 1; u <= n; ++u) {
-            fx[u] = c[u][1];
-            for (int v = 1; v <= n; ++v) {
-                fx[u] = min(fx[u], c[u][v]);
-            }
-        }
-        for (int v = 1; v <= n; ++v) {
-            fy[v] = c[1][v] - fx[1];
-            for (int u = 1; u <= n; ++u) {
-                fy[v] = min(fy[v], c[u][v] - fx[u]);
-            }
-        }
-        for (int u = 1; u <= n; ++u) {
-            start = u;
-            initBFS();
-            while (!finish) {
-                findAugPath();
-                if (!finish) subX_addY();
-            }
-            Enlarge();
-        }
-        int ans = 0;
-        for (int i = 1; i <= n; ++i) if (l[i] != 0 && c[i][l[i]] != inf) ans += c[i][l[i]];
-        return ans;
+        return make_pair(ans, cost);
     }
 };
-string s[55];
-bool vis[55][55];
+string a[55];
+bool vis[N][N];
 int32_t main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int n, m; cin >> n >> m;
-    for (int i = 0; i < n; i++) cin >> s[i];
-    Hungarian M(n * m + 1);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            function<void(int, int)> dfs = [&](int x, int y) {
-                if (s[x][y] == '#') return;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n, m; cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> a[i];
+	MCMF F(N - 10);
+    int s = N - 12, t = N - 13;
+    int cnt = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			function<void(int, int)> dfs = [&](int x, int y) {
+				if (a[x][y] == '#') return;
                 if (vis[x][y]) return;
                 vis[x][y] = 1;
-                M.add_edge(i * m + j + 1, x * m + y + 1, -(x - i + y - j));
-                if (x + 1 < n) dfs(x + 1, y);
-                if (y + 1 < m) dfs(x, y + 1);
-            };
-            if (s[i][j] == '#') continue;
-            if (s[i][j] == 'o') {
+				F.add_edge(cnt, x * m + y + 1 + 100, 1, -(x - i + y - j));
+				if (x + 1 < n) dfs(x + 1, y);
+				if (y + 1 < m) dfs(x, y + 1);
+			};
+			if (a[i][j] == '#') continue;
+			if (a[i][j] == 'o') {
+                F.add_edge(s, cnt, 1, 0);
                 memset(vis, 0, sizeof vis);
                 dfs(i, j);
+                ++cnt;
             }
-        }
-    }
-    cout << -M.maximum_matching() << '\n';
+            F.add_edge(i * m + j + 1 + 100, t, 1, 0);
+		}
+	}
+	cout << -F.solve(s, t).second << '\n';
     return 0;
 }
