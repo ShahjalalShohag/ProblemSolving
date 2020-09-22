5f4778111cdfa5ac0091d0d269ce85ae57eb4a75
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-09-23 03:12:00 +0600

    Add solution for problem `D - Christmas Trees` Link: https://codeforces.com/contest/1283/problem/D Tags: graphs, greedy, shortest paths, *1800 Ref: https://codeforces.com/contest/1283/submission/93517762

---
+++ codeforces/1283/D.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.buffer.readline
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+mp = {}
+q = []
+for i in a:
+	q.append(i)
+	mp[i] = 0
+ans, res, i = [], 0, 0
+while (len(ans) < m):
+	x = q[i]
+	for k in [-1, 1]:
+		if (x + k not in mp):
+			mp[x + k] = mp[x] + 1
+			res += mp[x + k]
+			q.append(x + k)
+			ans.append(x + k)
+		if (len(ans) == m):
+			break;
+	i += 1
+print(res)
+print(*ans)
+
+
