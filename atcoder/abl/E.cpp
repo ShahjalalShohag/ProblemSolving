80db1a09c379ea4b435728f0463bb7d093158d58
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-09-26 18:20:00 +0600

    Add solution for problem `E - Replace Digits` Link: https://atcoder.jp/contests/abl/tasks/abl_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abl/submissions/17033767

---
+++ atcoder/abl/E.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 2e5 + 9, mod = 998244353;
+
+template <const int32_t MOD>
+struct modint {
+    int32_t value;
+    modint() = default;
+    modint(int32_t value_) : value(value_) {}
+    inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+    inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+    inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+    modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; }
+    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (modint<MOD> other) const { return value == other.value; }
+    inline bool operator != (modint<MOD> other) const { return value != other.value; }
+    inline bool operator < (modint<MOD> other) const { return value < other.value; }
+    inline bool operator > (modint<MOD> other) const { return value > other.value; }
+};
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> istream & operator >> (istream & in, modint<MOD> &n) { return in >> n.value; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
+
+using mint = modint<mod>;
+
+mint s[N];
+struct ST {
+    #define lc (n << 1)
+    #define rc ((n << 1) | 1)
+   	mint t[4 * N];
+   	int lazy[4 * N];
+    ST() {
+        memset(t, 0, sizeof t);
+        memset(lazy, 0, sizeof lazy);
+    }
+    inline void push(int n, int b, int e) {
+        if (lazy[n] == 0) return;
+        t[n] = lazy[n] * (s[e] - s[b - 1]);
+        if (b != e) {
+            lazy[lc] = lazy[n];
+            lazy[rc] = lazy[n];
+        }
+        lazy[n] = 0;
+    }
+    inline mint combine(mint a,mint b) {
+        return a + b;
+    }
+    inline void pull(int n) {
+        t[n] = t[lc] + t[rc];
+    }
+    void build(int n, int b, int e) {
+        lazy[n] = 0;
+        if (b == e) {
+            t[n] = s[b] - s[b - 1];
+            return;
+        }
+        int mid = (b + e) >> 1;
+        build(lc, b, mid);
+        build(rc, mid + 1, e);
+        pull(n);
+    }
+    void upd(int n, int b, int e, int i, int j, int v) {
+        push(n, b, e);
+        if (j < b || e < i) return;
+        if (i <= b && e <= j) {
+            lazy[n] = v; //set lazy
+            push(n, b, e);
+            return;
+        }
+        int mid = (b + e) >> 1;
+        upd(lc, b, mid, i, j, v);
+        upd(rc, mid + 1, e, i, j, v);
+        pull(n);
+    }
+    mint query(int n, int b, int e, int i, int j) {
+        push(n, b, e);
+        if (i > e || b > j) return 0; //return null
+        if (i <= b && e <= j) return t[n];
+        int mid = (b + e) >> 1;
+        return combine(query(lc, b, mid, i, j), query(rc, mid + 1, e, i, j));
+    }
+}t;
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n, q; cin >> n >> q;
+	for (int i = 1; i <= n; i++) {
+		s[i] = s[i - 1] + mint(10).pow(n - i);
+	}
+	t.build(1, 1, n);
+	while (q--) {
+		int l, r, k; cin >> l >> r >> k;
+		t.upd(1, 1, n, l, r, k);
+		cout << t.query(1, 1, n, 1, n) << '\n';
+	}
+    return 0;
+}
