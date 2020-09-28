d6c57132178a89dd359b12e9ee689ee3272e0c29
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-09-28 20:01:00 +0600

    Add solution for problem `F - Number of Subsequences` Link: https://codeforces.com/contest/1426/problem/F Tags: combinatorics, dp, strings, *2000 Ref: https://codeforces.com/contest/1426/submission/94134427

---
+++ codeforces/1426/F.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int mod = 1e9 + 7;
+
+template <const int32_t MOD>
+struct modint {
+    int32_t value;
+    modint() = default;
+    modint(int32_t value_) : value(value_) {}
+    inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+    inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+    inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+    modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; }
+    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (modint<MOD> other) const { return value == other.value; }
+    inline bool operator != (modint<MOD> other) const { return value != other.value; }
+    inline bool operator < (modint<MOD> other) const { return value < other.value; }
+    inline bool operator > (modint<MOD> other) const { return value > other.value; }
+};
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> istream & operator >> (istream & in, modint<MOD> &n) { return in >> n.value; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
+
+using mint = modint<mod>;
+
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n; cin >> n;
+	string s; cin >> s;
+	map<char, int> l, r;
+	for (auto x: s) r[x]++;
+	int tot = r['?'];
+	mint ans = 0;
+	for (int i = 0; i < n; i++) {
+		r[s[i]]--;
+		int c = s[i] == '?';
+		if (s[i] == 'b' || s[i] == '?') {
+			ans += 1LL * l['a'] * r['c'] % mod * mint(3).pow(tot - c);
+			if (r['?']) ans += 1LL * l['a'] * r['?'] % mod * mint(3).pow(tot - c - 1);
+			if (l['?']) ans += 1LL * l['?'] * r['c'] % mod * mint(3).pow(tot - c - 1);
+			if (l['?'] && r['?']) ans += 1LL * l['?'] * r['?'] % mod * mint(3).pow(tot - c - 2);
+		}
+		l[s[i]]++;
+	}
+	cout << ans << '\n';
+    return 0;
+}
