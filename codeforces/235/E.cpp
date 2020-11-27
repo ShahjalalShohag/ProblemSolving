27b0c715d0fc655af0eb562543c02c2abb4f2946
Shahjalal Shohag <shahjalalshohag2014@gmail.com>
2020-11-27 22:28:00 +0600

    Add solution for problem `E - Number Challenge` Link: https://codeforces.com/contest/235/problem/E Tags: combinatorics, dp, implementation, math, number theory, *2600 Ref: https://codeforces.com/contest/235/submission/99734375

--- codeforces/235/E.cpp
+++ codeforces/235/E.cpp
@@ -3,33 +3,45 @@ using namespace std;
 
 const int N = 2010;
 
-int d[N], mob[N], f[N * N];
+int d[N];
+vector<int> p[N];
 int32_t main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-  int a, b, c; cin >> a >> b >> c;
-  mob[1] = 1;
-  for (int i = 1; i < N; i++) {
-    for (int j = i + i; j < N; j += i) {
-      mob[j] -= mob[i];
+  for (int i = 2; i < N; i++) {
+    int k = i;
+    for (int j = 2; j <= k; j++) {
+      if (k % j == 0) {
+        p[i].push_back(j);
+        while (k % j == 0) k /= j;
+      }
     }
   }
+  int a, b, c; cin >> a >> b >> c;
   for (int i = 1; i <= c; i++) {
     for (int j = i; j <= c; j += i) {
       d[i] += c / j;
-    }
-  }
-  for (int i = 1; i < N; i++) {
-    if (mob[i] == 0) continue;
-    for (int j = i; j < N * N; j += i) {
-      f[j] += mob[i] * d[i];
     }
   }
   long long ans = 0;
   for (int i = 1; i <= a; i++) {
     for (int j = 1; j <= b; j++) {
       if (__gcd(i, j) == 1) {
-        ans += 1LL * (a / i) * (b / j) * f[i * j];
+        vector<int> v;
+        for (auto x: p[i]) v.push_back(x);
+        for (auto x: p[j]) v.push_back(x);
+        int s = v.size(), cur = 0;
+        for (int mask = 0; mask < (1 << s); mask++) {
+          int mul = 1, sgn = 1;
+          for (int k = 0; k < s; k++) {
+            if (mask >> k & 1) {
+              mul *= v[k];
+              sgn *= -1;
+            }
+          }
+          if (mul <= c) cur += d[mul] * sgn;
+        }
+        ans += 1LL * (a / i) * (b / j) * cur;
       }
     }
   }
